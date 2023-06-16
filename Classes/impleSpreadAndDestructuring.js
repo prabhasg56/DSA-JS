686ed69388090b4db9378cbad40370d770bb5ad4
Prabhakar Mandal <prabhask856@gmail.com>
2023-06-16 16:50:23 +0530

    implement spread and rest

---
+++ Classes/impleSpreadAndDestructuring.js
@@ -0,0 +1,27 @@
+class Human {
+    //destructuring the array
+
+    constructor([name,age]){
+        this.age = age;
+        this.name = name;
+    }
+}
+
+class Person extends Human{
+    constructor(...age){
+        super(["Aman",age]);
+    }
+    printAge() {
+        console.log(this.age);
+        console.log(this.name)
+    }
+}
+
+const PerAge = [23,27,40,30];
+
+const prsnObj = new Person(...PerAge, 69, 90);
+prsnObj.printAge();
+
+//destructuring the properties of object
+const {age, name} = prsnObj;
+console.log(age);
